7e2ceb07d699635111e51733300e57180ab492de
Moumenh <mohanbali94@gmail.com>
2020-11-08 14:55:20 +0200

    finishing the login and signup

--- client/src/components/LoginPage/LogIn.jsx
+++ client/src/components/LoginPage/LogIn.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button, Input } from '@material-ui/core';
+import SignUp from './SignUp'
 
 import './Login.css'
 
@@ -9,33 +10,41 @@ class Login extends React.Component {
         super(props)
         this.state = {
             name: '',
-            password : ''
+            password: '',
+            signedUp: true
         }
     }
 
-    
-      handleChange = (e) => {
+
+    handleChange = (e) => {
         const { name, value } = e.target
-    
+
         this.setState({ [name]: value })
         console.log(value)
-      }
+    }
 
-      handleSignin = (e) => {
+    handleSignin = (e) => {
         e.preventDefault()
         this.props.postReqSignIn(this.state)
-      }
-    
-    
-      
+    }
+
+    signedUp = (e) => {
+        // e.preventDefault()
+        this.setState({ signedUp : !this.state.signedUp })
+    }
+
 
 
     render() {
-        const { name , password } = this.state
-        
+        const { name, password,signedUp } = this.state
+
         return (
-            <div className='login'>
-                <img className='loging__logo' src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'
+            
+            <div>
+                {
+                    signedUp ? 
+                    <div className='login'>
+                        <img className='loging__logo' src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'
                     alt='logo' />
                 <div className='loging__input'>
                     <Input className='login_in'
@@ -43,20 +52,20 @@ class Login extends React.Component {
                         name='name'
                         value={name}
                         onChange={this.handleChange}
-                        />
+                    />
                     <br />
                     <Input className='login_in'
                         type='password'
                         name='password'
                         value={password}
                         onChange={this.handleChange}
-                        />
+                    />
                     <Button type='submit' className='login_button' onClick={this.handleSignin}> Sign In </Button>
-                    <Button type='submit' className='login_button'> Sign Up </Button>
+                    <Button type='submit' className='login_button' onClick={this.signedUp}> Sign Up </Button>
                 </div>
-
-
-
+                    </div> : <SignUp signedUp={this.signedUp} />
+                }
+                
             </div>
         )
     }
--- client/src/components/LoginPage/SignUp.jsx
+++ client/src/components/LoginPage/SignUp.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Button, Input } from '@material-ui/core';
+import { Route } from 'react-router-dom'
 
 import './Login.css'
 
 
 class SignUp extends React.Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
             name: '',
             email: '',
@@ -15,41 +16,66 @@ class SignUp extends React.Component {
     }
     handleChange = (e) => {
         const { name, value } = e.target
-    
+
         this.setState({ [name]: value })
         console.log(value)
-      }
-    
-    
-      postReq = (obj) => {
+    }
+
+
+    postReq = (obj) => {
         // Simple POST request with a JSON body using fetch
         const requestOptions = {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(obj)
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(obj)
         };
         fetch('http://localhost:5000/signup', requestOptions)
-          .then(response => response.json())
-          .then(data => console.log(data))
-      }
+            .then(response => response.json())
+            .then(data => console.log(data))
+    }
+
+    handleClick = (e) => {
+        // e.preventDefault()
+        this.postReq(this.state)
+        this.props.signedUp()
+    }
 
 
 
 
     render() {
+        const { name, email, password } = this.state
         return (
 
             <div className='login'>
                 <img className='loging__logo' src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'
                     alt='logo' />
                 <div className='loging__input'>
-                    <Input className='login_in' />
+                    <Input className='login_in'
+                        placeholder='username ..'
+                        type='text'
+                        name='name'
+                        value={name}
+                        onChange={this.handleChange}
+                    />
                     <br />
-                    <Input className='login_in' />
+                    <Input className='login_in'
+                        placeholder='email ..'
+                        type='email'
+                        name='email'
+                        value={email}
+                        onChange={this.handleChange}
+                    />
                     <br />
-                    <Input className='login_in' />
+                    <Input className='login_in'
+                        placeholder='password ..'
+                        type='password'
+                        name='password'
+                        value={password}
+                        onChange={this.handleChange}
+                    />
 
-                    <Button type='submit' className='login_button'> Sign Up </Button>
+                    <Button type='submit' className='login_button' onClick={this.handleClick}> Sign Up </Button>
                 </div>
             </div>
         )
